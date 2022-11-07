e125991b7088aeb1d35803547ac1104d34468281
shiro4710 <54439127+shiro4710@users.noreply.github.com>
2022-11-07 23:03:22 +0900

    update: add `@emotion/react`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import React from "react";
+import { GlobalStyles } from "./styles/global";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <React.StrictMode>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </React.StrictMode>
+  );
 }
