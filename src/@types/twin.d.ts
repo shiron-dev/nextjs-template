4ef76a0a0febbf1fc8abba79a59aabcd84b7c56b
shiro4710 <54439127+shiro4710@users.noreply.github.com>
2022-11-08 15:36:43 +0900

    update: add prettier

--- src/@types/twin.d.ts
+++ src/@types/twin.d.ts
@@ -1,5 +1,5 @@
 import "twin.macro";
-import styledImport, { CSSProp, css as cssImport } from "styled-components";
+import styledImport, { css as cssImport } from "styled-components";
 
 declare module "twin.macro" {
   // The styled and css imports
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import tw from "twin.macro";
+import "twin.macro";
 
 export default function Home() {
   return <h1 tw={"text-center text-3xl"}>Hello, next.js!</h1>;
