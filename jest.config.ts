4a9f7eed753d883d73071e7f44054328fe2b80b6
shiro4710 <54439127+shiro4710@users.noreply.github.com>
2022-11-08 15:05:42 +0900

    fix: jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,10 @@ const config: Config = {
   verbose: true,
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   testEnvironment: "jest-environment-jsdom",
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  roots: ["<rootDir>/src"],
 };
 
 export default config;
