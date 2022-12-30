31433765d77263b53a15ed010f265fd8c2c4d059
shiro4710 <54439127+shiro4710@users.noreply.github.com>
2022-12-30 18:49:04 +0900

    update: use FaReact icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
 import "twin.macro";
+import { FaReact } from "react-icons/fa";
 
 export default function Home() {
-  return <h1 tw={"text-center text-3xl"}>Hello, next.js!</h1>;
+  return (
+    <div tw={"text-center text-3xl"}>
+      <h1>Hello, next.js!</h1>
+      <FaReact tw={"m-auto"} />
+    </div>
+  );
 }
